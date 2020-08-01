5b1781754bc989b5430eb165daa73e983e114550
Jake <a985724@gmail.com>
2020-08-01 17:09:36 +0800

    Finish weather app

---
+++ src/components/useWeatherApi.js
@@ -0,0 +1,107 @@
+import { useState, useEffect, useCallback } from 'react'
+
+const fetchCurrentWeather = (locationName) => {
+  // STEP 3-1：修改函式，把 fetch API 回傳的 Promise 直接回傳出去
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=${locationName}`,
+  )
+    .then(response => response.json())
+    .then(data => {
+      const locationData = data.records.location[0];
+
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (['WDSD', 'TEMP', 'HUMD'].includes(item.elementName)) {
+            neededElements[item.elementName] = item.elementValue;
+          }
+          return neededElements;
+        },
+        {},
+      );
+
+      // STEP 3-2：把取得的資料內容回傳出去，而不是在這裡 setWeatherElement
+      return {
+        observationTime: locationData.time.obsTime,
+        locationName: locationData.locationName,
+        temperature: weatherElements.TEMP,
+        windSpeed: weatherElements.WDSD,
+        humid: weatherElements.HUMD,
+      };
+    });
+};
+
+
+const fetchWeatherForecast = (cityName) => {
+// STEP 4-1：修改函式，把 fetch API 回傳的 Promise 直接回傳出去
+return fetch(
+  `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=${cityName}`,
+)
+  .then(response => response.json())
+  .then(data => {
+    const locationData = data.records.location[0];
+    const weatherElements = locationData.weatherElement.reduce(
+      (neededElements, item) => {
+        if (['Wx', 'PoP', 'CI'].includes(item.elementName)) {
+          neededElements[item.elementName] = item.time[0].parameter;
+        }
+        return neededElements;
+      },
+      {},
+    );
+
+    // STEP 4-2：把取得的資料內容回傳出去，而不是在這裡 setWeatherElement
+    return {
+      description: weatherElements.Wx.parameterName,
+      weatherCode: weatherElements.Wx.parameterValue,
+      rainPossibility: weatherElements.PoP.parameterName,
+      comfortability: weatherElements.CI.parameterName,
+    };
+  });
+};
+const useWeatherApi = (currentLocation) => {
+  const {locationName, cityName} = currentLocation;
+  const [weatherElement, setWeatherElement] = useState({
+    observationTime: new Date(),
+    locationName: '',
+    humid: 0,
+    temperature: 0,
+    windSpeed: 0,
+    description: '',
+    weatherCode: 0,
+    rainPossibility: 0,
+    comfortability: '',
+    isLoading: true,
+  });
+  
+  const fetchData = useCallback(() => {
+    const fetchingData = async () => {
+      const [currentWeather, weatherForecast] = await Promise.all([
+        fetchCurrentWeather(locationName),
+        fetchWeatherForecast(cityName),
+      ]);
+
+      setWeatherElement({
+        ...currentWeather,
+        ...weatherForecast,
+        isLoading: false,
+      });
+    };
+
+    setWeatherElement(prev => ({
+      ...prev,
+      isLoading: true,
+    }));
+
+    fetchingData();
+  }, [locationName, cityName]);
+
+
+useEffect(() => {
+  fetchData();
+}, [fetchData]);
+
+return [weatherElement, fetchData];
+
+}
+
+export default useWeatherApi;
